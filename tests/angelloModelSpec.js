78087b11acfd49324a10fac2e3a66e6cc6eea4ec
Michael Pope <michael@dtcorp.com.au>
2013-09-24 15:47:39 +1000

    Add another test to check if there is a 'To Do' status item

--- tests/angelloModelSpec.js
+++ tests/angelloModelSpec.js
@@ -12,5 +12,15 @@ describe("Service: angelloModel", function(){
 		it('should return seven different statuses', function(){
 			expect(modelService.getStatuses().length).toBe(7);
 		});
+
+		it('should have a status named "To Do"', function(){
+			expect(modelService.
+				   getStatuses().
+				   map(function(status) {// get just the name of each status
+						   return status.name;
+				   })).
+				   toContain("To Do");
+		});
 	});
+
 });
