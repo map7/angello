a6bad839d5d10c0c5ce83614da11585ab47e2041
Michael Pope <michael@dtcorp.com.au>
2013-09-24 11:38:25 +1000

    Spice up the story - Add more fields

--- app/app.js
+++ app/app.js
@@ -3,12 +3,35 @@ var myModule = angular.module('Angello', [])
 myModule.controller('MainCtrl', function($scope) {
 	// Add content
 	$scope.stories = [
-		{title: 'Story 00', description: 'Description pending.'},
-		{title: 'Story 01', description: 'Description pending.'},
-		{title: 'Story 02', description: 'Description pending.'},
-		{title: 'Story 03', description: 'Description pending.'},
-		{title: 'Story 04', description: 'Description pending.'},
-		{title: 'Story 05', description: 'Description pending.'}
+		{
+			title: 'Story 00',
+			description: 'Description pending.',
+			criteria: 'Criteria pending',
+			status:'To Do',
+			type:'Feature',
+			reporter:'Lukas Ruebbelke',
+			assignee:'Brian Ford'
+		},
+		{
+			title: 'Story 01',
+			description: 'Description pending.'
+		},
+		{
+			title: 'Story 02',
+			description: 'Description pending.'
+		},
+		{
+			title: 'Story 03',
+			description: 'Description pending.'
+		},
+		{
+			title: 'Story 04',
+			description: 'Description pending.'
+		},
+		{
+			title: 'Story 05',
+			description: 'Description pending.'
+		}
 	];
 
 	// Setting current Story
