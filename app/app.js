3a2cd521cffaedb297b08a3630e5759ecbbd0c32
Michael Pope <michael@dtcorp.com.au>
2013-09-24 11:22:54 +1000

    Getting the first binding working

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 var myModule = angular.module('Angello', [])
 
 myModule.controller('MainCtrl', function($scope) {
+	// Add content
 	$scope.stories = [
 		{title: 'Story 00', description: 'Description pending.'},
 		{title: 'Story 01', description: 'Description pending.'},
@@ -9,4 +10,12 @@ myModule.controller('MainCtrl', function($scope) {
 		{title: 'Story 04', description: 'Description pending.'},
 		{title: 'Story 05', description: 'Description pending.'}
 	];
+
+	$scope.currentStory;
+
+	$scope.setCurrentStory = function(story) {
+		$scope.currentStory = story;
+	};
+
+	
 });
